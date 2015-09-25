15e06d9970978f8020fb339c078307cf7508fcca
ZachNo <ZachNo@users.noreply.github.com>
2015-09-25 13:10:37 -0400

    Added pack for WR bricks

---
+++ brick_WRBuilding/server.cs
@@ -0,0 +1,44 @@
+//By ZSNO
+datablock fxDTSBrickData(SurviveMilitaryBrick)
+{
+	brickFile = "base/data/bricks/flats/8x8F.blb";
+	category = "Special";
+	subCategory = "What Remains";
+	uiName = "Military Loot Spawn ";
+};
+datablock fxDTSBrickData(SurviveMedicalBrick)
+{
+	brickFile = "base/data/bricks/flats/8x8F.blb";
+	category = "Special";
+	subCategory = "What Remains";
+	uiName = "Medical Loot Spawn ";
+};
+datablock fxDTSBrickData(SurviveGasSBrick)
+{
+	brickFile = "base/data/bricks/flats/8x8F.blb";
+	category = "Special";
+	subCategory = "What Remains";
+	uiName = "Gas Station Loot Spawn ";
+};
+datablock fxDTSBrickData(SurviveGeneralBrick)
+{
+	brickFile = "base/data/bricks/flats/8x8F.blb";
+	category = "Special";
+	subCategory = "What Remains";
+	uiName = "General Loot Spawn ";
+};
+datablock fxDTSBrickData(SurviveFoodBrick)
+{
+	brickFile = "base/data/bricks/flats/8x8F.blb";
+	category = "Special";
+	subCategory = "What Remains";
+	uiName = "Food Loot Spawn ";
+};
+datablock fxDTSBrickData(SurviveVehicleBrick)
+{
+	brickFile = "base/data/bricks/special/vehicleSpawn.blb";
+	category = "Special";
+	subCategory = "What Remains";
+	uiName = "WRVehicle Spawn ";
+	specialBrickType = "vehicleSpawn";
+};
